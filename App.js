46f973fce96c02566404e51ce619d398e7e037e6
0xnunana <kudayapaul@gmail.com>
2023-05-10 19:19:33 +0000

    FlatList and SectionList

--- App.js
+++ App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, SectionList, FlatList } from 'react-native';
+import ColorBox from './components/ColorBox';
+const FOODSS = [
+  'Apples',
+  'Broccoli',
+  'Cookies',
+  'Doritos',
+  'Eclairs'
+];
+const FOODS = [
+  { title: 'Healthy', data: ['Apples', 'Broccoli'] },
+  { title: 'Not so Healthy', data: ['Cookies', 'Doritos', 'Eclairs'] },
+];
 export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 10 }}>
-      <View>
-        <Text style={styles.margina}>Styling Exercise by 0xNunana</Text></View>
 
-      <View style={[styles.container, { backgroundColor: '#2aa198' }]}>
-        <Text>Cyan</Text>
+      <View>
+        <Text style={styles.margina}>Styling Exercise by 0xNunana</Text>
+        <View style={[styles.container, { backgroundColor: '#2aa198' }]}>
+          <Text style={{ color: 'white' }}>Cyan</Text>
+        </View>
+        <View style={[styles.container, { backgroundColor: '#268bd2' }]}>
+          <Text style={{ color: 'white' }}>Blue</Text>
+        </View>
+        <View style={[styles.container, { backgroundColor: '#d33682' }]}>
+          <Text style={{ color: 'white' }}>Magenta</Text>
+        </View>
+        <View style={[styles.container, { backgroundColor: '#cb4b16', }]}>
+          <Text style={{ color: 'white' }}>Orange</Text>
+        </View>
+        <SectionList
+          sections={FOODS}
+          keyExtractor={e => e}
+          renderItem={({ item }) => <ColorBox name={item} />}
+          renderSectionHeader={({ section }) => (<Text style={{ fontSize: 18, fontWeight: 'bold', backgroundColor: 'yellow' }}>{section.title}</Text>)}
+
+        />
+
+        <FlatList
+
+          data={FOODSS}
+          keyExtractor={item => item}
+          renderItem={({ item }) => <ColorBox name={item} />}
+        />
+
       </View>
-      <View style={[styles.container, { backgroundColor: '#268bd2' }]}>
-        <Text>Blue</Text>
-      </View>
-      <View style={[styles.container, { backgroundColor: '#d33682' }]}>
-        <Text>Magenta</Text>
-      </View>
-      <View style={[styles.container, { backgroundColor: '#cb4b16' }]}>
-        <Text>Orange</Text>
-      </View>
+
+
 
       {/* <StatusBar style="auto" /> */}
     </SafeAreaView>
@@ -31,8 +60,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
 
-
-    paddingVertical: 5,
+    marginBottom: 5,
+    paddingVertical: 10,
     alignItems: 'center',
     justifyContent: "center",
 
@@ -40,6 +69,11 @@ const styles = StyleSheet.create({
   ,
   margina: {
     backgroundColor: "lavender",
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5,
+    marginTop: 5,
+    paddingVertical: 10
 
 
 
