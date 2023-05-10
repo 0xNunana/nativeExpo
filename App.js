19650da3b94ceb700ce273dde3ff864f4d340e44
0xnunana <kudayapaul@gmail.com>
2023-05-10 08:39:53 +0000

    styling exercise

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingHorizontal: 10 }}>
+      <View>
+        <Text style={styles.margina}>Styling Exercise by 0xNunana</Text></View>
+
+      <View style={[styles.container, { backgroundColor: '#2aa198' }]}>
+        <Text>Cyan</Text>
+      </View>
+      <View style={[styles.container, { backgroundColor: '#268bd2' }]}>
+        <Text>Blue</Text>
+      </View>
+      <View style={[styles.container, { backgroundColor: '#d33682' }]}>
+        <Text>Magenta</Text>
+      </View>
+      <View style={[styles.container, { backgroundColor: '#cb4b16' }]}>
+        <Text>Orange</Text>
+      </View>
+
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+
+    paddingVertical: 5,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: "center",
+
+  }
+  ,
+  margina: {
+    backgroundColor: "lavender",
+
+
+
+  }
+})
