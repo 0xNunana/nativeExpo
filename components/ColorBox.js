5ba3b8f1956bfc99ce7f2f4b2dd637715ce1b28e
0xnunana <kudayapaul@gmail.com>
2023-05-11 22:38:05 +0000

    colored bg with text

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ColorBox = ({ name }) => {
+const ColorBox = ({ colorName, hexCode }) => {
     return (
-        <View style={styles.box} >
-            <Text style={{ color: 'white' }} >{name}</Text>
+        <View style={[styles.box, { backgroundColor: hexCode, }]} >
+            <Text style={{ color: 'white' }} >{colorName}</Text>
         </View>
     )
 }
@@ -13,8 +13,7 @@ export default ColorBox
 
 const styles = StyleSheet.create({
     box: {
-        backgroundColor: 'purple',
-        marginVertical: 10,
+
         paddingVertical: 5,
         paddingHorizontal: 10,
         justifyContent: 'center',
